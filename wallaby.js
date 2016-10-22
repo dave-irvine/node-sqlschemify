50341a7ca5421dba42f9643ac021759b3a6c709e
Dave Irvine <dave@daveirvine.me>
2016-10-22 20:58:32 +0100

    Add wallaby config

---
+++ wallaby.js
@@ -0,0 +1,27 @@
+var babel = require('babel-core');
+
+module.exports = function (wallaby) {
+    return {
+        files: [
+            'src/**/*.js'
+        ],
+
+        tests: [
+            'test/**/*.test.js'
+        ],
+
+        compilers: {
+            '**/*.js': wallaby.compilers.babel({
+                babel: babel,
+                presets: ['es2015'],
+                plugins: ['transform-runtime']
+            })
+        },
+        env: {
+            type: 'node'
+        },
+        workers: {
+            recycle: true
+        }
+    };
+};
